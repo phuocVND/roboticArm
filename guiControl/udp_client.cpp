654271072974069d81b0040bac618e81dde57a6a
Duy Phuoc <vonguyenduyphuoc2001@gmai.com>
2025-07-09 05:49:44 +0700

    fix udp for win

--- guiControl/udp_client.cpp
+++ guiControl/udp_client.cpp
@@ -1,59 +1,80 @@
-// #include "udp_client.h"
-// #include <cstring>      // memset
-// #include <arpa/inet.h>  // inet_pton, sockaddr_in
-// #include <thread>
-// #include <unistd.h>     // close
-// #include <iostream>
+#include "udp_client.h"
+#include <cstring>
+#include <thread>
+#include <iostream>
 
-// udp_client::udp_client(const std::string& ip, int port, Parameter *parameter)
-// {
-//     m_socket = socket(AF_INET, SOCK_DGRAM, 0);
-//     if (m_socket < 0) {
-//         perror("Socket creation failed");
-//         return;
-//     }
+udp_client::udp_client(const std::string& ip, int port, Parameter *parameter)
+    : parameter(parameter)
+{
+#ifdef _WIN32
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
+        m_socket = -1;
+        return;
+    }
+#endif
 
-//     memset(&m_server_addr, 0, sizeof(m_server_addr));
-//     m_server_addr.sin_family = AF_INET;
-//     m_server_addr.sin_port = htons(port);
+    m_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (m_socket < 0) {
+        perror("Socket creation failed");
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return;
+    }
 
-//     if (inet_pton(AF_INET, ip.c_str(), &m_server_addr.sin_addr) <= 0) {
-//         perror("Invalid address");
-//         close(m_socket);
-//         m_socket = -1;
-//     }
-// }
+    std::memset(&m_server_addr, 0, sizeof(m_server_addr));
+    m_server_addr.sin_family = AF_INET;
+    m_server_addr.sin_port = htons(port);
 
-// udp_client::~udp_client()
-// {
-//     if (m_socket >= 0)
-//         close(m_socket);
-// }
+    if (inet_pton(AF_INET, ip.c_str(), &m_server_addr.sin_addr) <= 0) {
+        perror("Invalid address");
+#ifdef _WIN32
+        closesocket(m_socket);
+        WSACleanup();
+#else
+        close(m_socket);
+#endif
+        m_socket = -1;
+    }
+}
 
-// bool udp_client::send_data(ParameterAngle* data)
-// {
-//     if (m_socket < 0)
-//         return false;
+udp_client::~udp_client()
+{
+    if (m_socket >= 0) {
+#ifdef _WIN32
+        closesocket(m_socket);
+        WSACleanup();
+#else
+        close(m_socket);
+#endif
+    }
+}
 
-//     ssize_t sent = sendto(m_socket, data, sizeof(ParameterAngle), 0,
-//                           (struct sockaddr*)&m_server_addr, sizeof(m_server_addr));
-//     return sent >= 0;
-// }
+bool udp_client::send_data(ParameterAngle* data)
+{
+    if (m_socket < 0) return false;
 
-// void udp_client::startConnect() {
-//     while (true) {
-//         if(parameter->isChanged) {
-//             parameterAngle = parameter->get_parameter();
-//             ParameterAngle data = *parameterAngle;
+    ssize_t sent = sendto(m_socket, (const char*)data, sizeof(ParameterAngle), 0,
+                          (struct sockaddr*)&m_server_addr, sizeof(m_server_addr));
+    return sent >= 0;
+}
 
-//             data.angle1 = htons(data.angle1);
-//             data.angle2 = htons(data.angle2);
-//             data.angle3 = htons(data.angle3);
-//             data.angle4 = htons(data.angle4);
-//             data.angle5 = htons(data.angle5);
-//             data.angle6 = htons(data.angle6);
-//             send_data(&data);
-//         }
-//         std::this_thread::sleep_for(std::chrono::milliseconds(10));
-//     }
-// }
+void udp_client::startConnect() {
+    while (true) {
+        if (parameter->isChanged) {
+            parameterAngle = parameter->get_parameter();
+            ParameterAngle data = *parameterAngle;
+
+            data.angle1 = htons(data.angle1);
+            data.angle2 = htons(data.angle2);
+            data.angle3 = htons(data.angle3);
+            data.angle4 = htons(data.angle4);
+            data.angle5 = htons(data.angle5);
+            data.angle6 = htons(data.angle6);
+
+            send_data(&data);
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
--- guiControl/udp_client.h
+++ guiControl/udp_client.h
@@ -1,23 +1,42 @@
-// #ifndef UDP_CLIENT_H
-// #define UDP_CLIENT_H
+#ifndef UDP_CLIENT_H
+#define UDP_CLIENT_H
 
-// #include "parameter.h"
-// #include <netinet/in.h>
-// #include <string>
+#include "parameter.h"
+#include <string>
 
-// class udp_client
-// {
-// public:
-//     udp_client(const std::string& ip, int port, Parameter *parameter);
-//     ~udp_client();
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+typedef int socklen_t;
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
 
-//     bool send_data(ParameterAngle *data);
-//     void startConnect();
-// private:
-//     Parameter *parameter;
-//     ParameterAngle *parameterAngle;
-//     int m_socket;
-//     struct sockaddr_in m_server_addr;
-// };
 
-// #endif // UDP_CLIENT_H
+class udp_client
+{
+public:
+    udp_client(const std::string& ip, int port, Parameter *parameter);
+    ~udp_client();
+
+    bool send_data(ParameterAngle *data);
+    void startConnect();
+
+private:
+    Parameter *parameter;
+    ParameterAngle *parameterAngle;
+    int m_socket;
+    struct sockaddr_in m_server_addr;
+
+#ifdef _WIN32
+    WSADATA wsaData;
+#endif
+};
+
+#endif // UDP_CLIENT_H
